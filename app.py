c1571b59ad5947b0370f45257ea862c0aa3ad3a3
Rahul B <93589189+rahulbala0@users.noreply.github.com>
2024-06-21 16:38:50 -0400

    functions

--- app.py
+++ app.py
@@ -8,3 +8,11 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+def pounds_to_kg(pounds):
+    return pounds / 2.205
+
+def inches_to_meters(feet, inches):
+    total_inches = feet * 12 + inches
+    return total_inches / 39.37
